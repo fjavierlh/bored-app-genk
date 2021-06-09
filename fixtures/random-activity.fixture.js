2817ae184305eb81b08b585ae7cfca43390290b2
fjavierlh <fjavierlh@gmail.com>
2021-06-09 19:08:42 +0000

    test: created random activity fixture to mock tests

---
+++ fixtures/random-activity.fixture.js
@@ -0,0 +1,11 @@
+export const RANDOM_ACTIVITY = {
+    data: {
+        activity: 'Write a poem',
+        type: 'recreational',
+        participants: 1,
+        price: 0,
+        link: '',
+        key: '8688620',
+        accessibility: 0
+    }
+};
