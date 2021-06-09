133e652bcf7daf0aa8c48e25003978b9816aa398
fjavierlh <fjavierlh@gmail.com>
2021-06-09 18:55:37 +0000

    test: added suite test to ActivityRepository

---
+++ src/app/repositories/activity.repository.spec.js
@@ -0,0 +1,31 @@
+import axios from "axios";
+import { ActivityRepository } from "./activity.respository";
+import { RANDOM_ACTIVITY } from '../../../fixtures/random-activity.fixture';
+jest.mock('axios');
+
+describe("ActivityRepository", () => {
+  
+    beforeEach(() => {
+    axios.mockClear();
+  });
+
+  it("Should return an object with a ramdom activity", async () => {
+
+    axios.get = jest.fn().mockResolvedValue(RANDOM_ACTIVITY);
+
+    const activityRepository = new ActivityRepository();
+    const expectedObjectKeys = [
+      "accessibility",
+      "activity",
+      "key",
+      "link",
+      "participants",
+      "price",
+      "type",
+    ];
+    const resultGet = await activityRepository.getRandomActivity();
+    const resultObjectKeys = Object.keys(resultGet.data).sort();
+
+    expect(resultObjectKeys).toEqual(expectedObjectKeys);
+  });
+});
