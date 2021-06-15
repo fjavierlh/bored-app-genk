a09d3126a1ad6448a944e57d11b19d42bff5d1e0
fjavierlh <fjavierlh@gmail.com>
2021-06-15 14:47:53 +0000

    chore: imported ActivityOptionsPanel and MainLayout

--- src/main.js
+++ src/main.js
@@ -2,3 +2,5 @@ import './main.css';
 import { router } from './router';
 import { WelcomePage } from './app/pages/welcome.page';
 import { MainPage } from './app/pages/main.page';
+import { ActivityOptionsPanel } from './app/components/activity-options-panel.component';
+import { MainLayout }  from './app/layout/main.layout';
