5bcc10bcf34e3290a712b451cc4761759513a8c1
fjavierlh <fjavierlh@gmail.com>
2021-06-17 16:24:49 +0000

    refactor: removd some unnecessary styles and defined properties

--- src/app/pages/main.page.js
+++ src/app/pages/main.page.js
@@ -1,39 +1,41 @@
 import { html, css, LitElement } from "lit";
-import  commonPageStyles  from "./common.styles";
-
 
 export class MainPage extends LitElement {
-  constructor() {
-    super();
-  }
+  
 
   static get styles() {
     return css`
-    :host {
-      width: 100%;
-      display: flex;
+     :host {
+      display: block;
       min-height: 100%;
     }
     
-    :host > div {
-      flex: 1 100%;
-      padding: 3em;
+    .main-content {
+      display: block;
+      height: 100%;
+      
     }
 
     `;
   }
 
+  static get properties()  {
+    return {
+      image: { type: String}
+    }
+  }
+
+  constructor() {
+    super();
+  }
+  
   connectedCallback() {
     super.connectedCallback();
   }
 
   render() {
     return html`
-    <div>
-      <h1>Main page</h1>
-      <a href="/">Go back</a>
-      <activity-options-panel></activity-options-panel>
-    </div>
+        <activity-card></activity-card>
     `;
   }
 }
