161a9a5512765c5a0ffa5ae1a747ed6ca0fb0522
fjavierlh <fjavierlh@gmail.com>
2021-06-17 16:34:57 +0000

    chore: added ActivityCard to main.js

--- src/main.js
+++ src/main.js
@@ -3,4 +3,5 @@ import { router } from './router';
 import { WelcomePage } from './app/pages/welcome.page';
 import { MainPage } from './app/pages/main.page';
 import { ActivityOptionsPanel } from './app/components/activity-options-panel.component';
+import { ActivityCard } from './app/components/activity-card.component';
 import { MainLayout }  from './app/layout/main.layout';
