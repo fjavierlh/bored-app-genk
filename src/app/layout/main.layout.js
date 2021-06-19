998adfb66876a0842a3cfd5dc4355bc0507348ea
fjavierlh <fjavierlh@gmail.com>
2021-06-19 23:37:39 +0000

    refactor: renamed css selectors and added events listeners in firstUpdated meethod

--- src/app/layout/main.layout.js
+++ src/app/layout/main.layout.js
@@ -5,46 +5,72 @@ export class MainLayout extends LitElement {
     return css`
       :host {
         display: block;
-        width: 100%;
+        min-height: 100%;
       }
 
-      div {
+      .container {
         min-height: 100vh;
         display: grid;
-        grid-template-rows: 5em auto;
+        grid-template-rows: 5em auto 1.5em;
         grid-template-areas:
           "h"
-          "s";
+          "s"
+          "f";
       }
 
-      header {
-        z-index: 0;
-        /* background-color: teal; */
+      #main-header {
+        display: flex;
+        flex-flow: row wrap;
         grid-area: h;
       }
-      header,
-      footer {
-        background-color: #fff;
+      #main-header,
+      #main-footer {
+        background-color: var(--color-primary);
         z-index: 1;
       }
 
+      #main-header {
+        box-shadow: 1px -25px 50px;
+      }
+
       #nav {
-        z-index: 3;
-        display: block;
+        z-index: 4;
+        display: flex;
+        flex-flow: column wrap;
         width: 100vw;
         min-height: 100vh;
         position: fixed;
         top: 0;
-        left: -100%;
-        background-color: #38a1ff;
+        left: -110%;
+        background-color: var(--color-secondary);
         transition: left 0.4s ease-in-out;
+      }
+
+      #nav ul {
+        list-style-type: none;
+        margin: 0;
+        padding: 0;
+      }
+
+      .nav-link {
+        display: block;
+        text-decoration: none;
+        color: var(--color-primary);
+        font-size: 2.5rem;
+        line-height: 2;
+        text-align: center;
+        transition: color 0.5s ease;
+      }
+
+      .nav-link:active {
+        color: var(--color-tertiary);
       }
 
       .open {
         left: 0% !important;
       }
 
-      .toggle {
+      .toggle-nav {
         width: 4em;
         height: 4em;
         background: none;
@@ -56,64 +82,86 @@ export class MainLayout extends LitElement {
         outline: inherit;
       }
 
-      .toggle::after {
+      .toggle-nav::after {
         display: block;
         font-size: 3em;
         position: fixed;
         top: 0.3em;
       }
 
-      .toggle-open::after,
-      .toggle-close::after {
+      .toggle-nav-open::after,
+      .toggle-nav-close::after {
         left: 0.5em;
       }
-      .toggle-open::after {
+
+      .toggle-nav-open::after {
         content: "☰";
       }
 
-      .toggle-close::after {
-        color: #fff;
-        content: "✖";
-      }
-      .toggle-close::after:hover {
-        background-color: red;
+      .toggle-nav-close::after {
+        color: var(--color-primary);
         content: "✖";
       }
 
-      section {
-        /* background-color: salmon; */
+      #main-section {
         grid-area: s;
       }
-      aside {
-        display: none;
-        /* background-color: skyblue; */
-        grid-area: a;
-      }
-      footer {
-        /* background-color: slategrey; */
+
+      #main-footer {
+        font-family: var(--font-primary);
+        font-size: 0.8rem;
+        display: flex;
+        justify-content: center;
+        align-items: center;
         grid-area: f;
+        box-shadow: 1px 25px 50px;
+      }
+
+      #main-footer a {
+        text-decoration: none;
+        padding-left: 0.5em;
+        color: var(--color-secondary);
       }
 
       @media (min-width: 812px) {
-        .toggle {
+        .container {
+          grid-template-columns: repeat(6, 1fr);
+          grid-template-rows: 5em auto 1.5em;
+          grid-template-areas:
+            "h h h h h h"
+            "s s s s s s"
+            "f f f f f f";
+        }
+
+        .toggle-nav {
           display: none;
         }
 
         #nav {
           width: auto;
-          min-height: auto;
-          position: initial;
-
+          flex-flow: row wrap;
+          justify-content: flex-start;
+          align-items: center;
+          min-height: 5em;
+          position: fixed;
+          top: 0%;
+          left: 0%;
+          background-color: var(--color-primary);
           grid-area: n;
         }
 
-        div {
-          grid-template-columns: repeat(12, 1fr);
-          grid-template-rows: 50px auto 60px;
-          grid-template-areas:
-            "h h h h h h h n n n n n"
-            ". s s s s s s a a a a ."
-            "f f f f f f f f f f f f";
+        #nav ul li {
+          display: inline-block;
+          margin-right: 1.5em;
+        }
+
+        #nav ul li:first-child {
+          margin-left: 3em;
+        }
+
+        .nav-link {
+          color: var(--color-tertiary);
+          font-size: 1.2rem;
         }
       }
     `;
@@ -122,56 +170,56 @@ export class MainLayout extends LitElement {
   static get properties() {
     return {
       openNav: { type: Boolean },
+      links: { type: Array },
     };
   }
 
   constructor() {
     super();
     this.openNav = false;
+    this.nav = null;
+  }
+
+  firstUpdated() {
+    this.navLinks = [...this.renderRoot.querySelectorAll("#nav ul li a")];
+    console.log(this.navLinks);
+    this.navLinks.forEach((link) =>
+      link.addEventListener("click", (e) => this._toggleMenu(e))
+    );
+
+    this.toggleNav = this.renderRoot.querySelector(".toggle-nav");
+    this.toggleNav.addEventListener("click", (e) => this._toggleMenu(e));
   }
 
   _toggleMenu() {
+    console.log(window.width);
     this.openNav = !this.openNav;
   }
 
   render() {
     return html`
-      <div>
-        <header></header>
-
+      <div class="container">
+        <header id="main-header"></header>
         <nav part="nav" id="nav" class="${this.openNav ? "open" : ""}">
           <button
-            class="toggle toggle${!this.openNav ? "-open" : "-close"}"
-            @click=${(e) => {
-              this._toggleMenu(e);
-            }}
+            class="toggle-nav toggle-nav${!this.openNav ? "-open" : "-close"}"
           ></button>
           <ul>
             <li>
-              <a
-                href="/"
-                @click=${(e) => {
-                  this._toggleMenu(e);
-                }}
-                >Welcome</a
-              >
+              <a class="nav-link" href="/">Welcome</a>
             </li>
             <li>
-              <a
-                href="/main-apge"
-                @click=${(e) => {
-                  this._toggleMenu(e);
-                }}
-                >Main page</a
-              >
+              <a class="nav-link" href="/main-page">Main page</a>
             </li>
           </ul>
-          <footer part="footer">Made with love by @fjavierlh</footer>
         </nav>
-        <section part="section">
+
+        <section id="main-section" part="section">
           <slot></slot>
         </section>
-        <aside part="aside"></aside>
+        <footer id="main-footer" part="footer">
+          Made with 💙 by <a href="https://github.com/fjavierlh"> @fjavierlh</a>
+        </footer>
       </div>
     `;
   }
